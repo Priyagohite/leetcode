4775f6345ad3f44946ddf10069f330ad48672020
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2024-02-19 23:58:48 +0530

    Time: 1 ms (49.23%), Space: 40.5 MB (91.55%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) 
+    {
+     if(n==0)
+     {
+         return false;
+     }
+        while(n!=1)
+        {
+            if(n%2!=0)
+            {
+                return false;
+            }
+            else
+            {
+                n=n/2;
+            }
+        }
+        return true;
+    }
+}
