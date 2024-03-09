d1bba8ff82937b0ac9b1b360f7c0a4da73ec6d25
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2024-03-09 18:47:09 +0530

    Time: 14 ms (24.59%), Space: 64.1 MB (13.78%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        int result = -1;
+        for(int num : nums1)
+        {
+            set.add(num);
+        }
+        for(int num : nums2)
+        {
+            if(set.contains(num))
+            {
+                result = num;
+                break;
+            }
+        }
+        return result;
+    }
+}
