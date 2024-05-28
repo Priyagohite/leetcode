7db431ee0b938213e168116dbf0a775d6f36b05d
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2024-05-29 04:08:18 +0530

    Time: 7 ms (73.63%), Space: 43.1 MB (40.09%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int equalSubstring(String s, String t, int maxCost) {
+         int n = s.length();
+        
+        int maxLen = 0;
+        int currCost = 0;
+        
+        int i = 0, j = 0;
+        while (j < n) {
+            currCost += Math.abs(s.charAt(j) - t.charAt(j));
+            
+            while (currCost > maxCost) {
+                currCost -= Math.abs(s.charAt(i) - t.charAt(i));
+                i++;
+            }
+            
+            maxLen = Math.max(maxLen, j - i + 1);
+            j++;
+        }
+        
+        return maxLen;
+    }
+}
